1999b2170ab131208670b21bb9a48fb3ae57c772
chhavivohra <chhavivohra82@gmail.com>
2022-03-30 13:57:32 +0530

    add a moral story

---
+++ text.js
@@ -0,0 +1,13 @@
+People have been coming to the wise man, complaining about the same problems every time.
+
+ One day he told them a joke and everyone roared in laughter.
+
+After a couple of minutes, he told them the same joke and only a few of them smiled.
+
+When he told the same joke for the third time no one laughed anymore.
+
+The wise man smiled and said:
+
+Worrying won’t solve your problems, it’ll just waste your time and energy.
+
+ 
